e5a6aaa3c757256ba227d7a38ae1b90d2163bed6
ogabriel <gabrieloliver8991@gmail.com>
2019-06-22 00:59:11 -0300

    add ecto lessons

---
+++ ecto/basic.exs
@@ -0,0 +1,11 @@
+# ECTO
+# is an official elixir project prividing database wrapper and integrated query language
+# with it, we are able to create migrations, define schemas, insert and update records and query them
+#
+# ADAPTERS
+# PostgreSQL
+# MySQL
+# SQLite
+#
+# Example
+# mix new fjkkk
