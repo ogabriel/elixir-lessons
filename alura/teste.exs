888b28a822b569ed9b1fcb172ae11533578705ca
ogabriel <gabrieloliver8991@gmail.com>
2022-07-29 17:07:26 -0300

    update teste

--- alura/teste.exs
+++ alura/teste.exs
@@ -1,9 +1,19 @@
 defmodule MeuModulo do
+  import IO, only: [puts: 1]
+  import Kernel, except: [inspect: 1]
+
   def ola_mundo do
-    IO.puts("Olá, mundo!")
+    puts("Olá, mundo!")
+  end
+
+  def inspect(parametro) do
+    puts("começando")
+    puts(parametro)
+    puts("terminando")
   end
 end
 
-iex teste.exs
-MeuModulo.ola_mundo
-r(MeuModulo)
+# iex teste.exs ou
+# import_file "teste.exs"
+# MeuModulo.ola_mundo
+# r(MeuModulo)
