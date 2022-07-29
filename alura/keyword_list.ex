1e874444a90044dc0c1da2a8fc5e9596bb5f174d
ogabriel <gabrieloliver8991@gmail.com>
2022-07-29 16:25:59 -0300

    add alura keyword_list

---
+++ alura/keyword_list.ex
@@ -0,0 +1,7 @@
+keyword_list = [um: 1, dois: 2, tres: 3]
+
+keyword_list = [{:um, 1,}, {:dois, 2}, {:tres, 3}]
+
+[um: 1, dois: 2, tres: 3] == [{:um, 1,}, {:dois, 2}, {:tres, 3}]
+
+# equal to map, but needs a atom as key
