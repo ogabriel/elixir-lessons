6ed452e50184a8b864119ca79269377d9578222d
ogabriel <gabrieloliver8991@gmail.com>
2022-11-11 16:01:19 -0300

    func anonimas

---
+++ alura/funcao.exs
@@ -0,0 +1,9 @@
+# is_number(1)
+# i_n = &is_number/1
+# i.n.(2)
+# lista = [1, 2, 3]
+# Enum.map(lista, fn (numero) -> numero * 2 end)
+# Enum.map(lista, &multiplicar_por_dois/1)
+# Enum.map(lista, &(&1 * 2))
+# soma_2 = &(&1 + 2)
+# soma_2.(6)
