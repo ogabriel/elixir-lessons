72f91e94646f4609fb458ed74bdb0b008230535b
ogabriel <gabrieloliver8991@gmail.com>
2022-11-11 16:21:06 -0300

    add funcions

--- alura/math.exs
+++ alura/math.exs
@@ -4,4 +4,7 @@ defmodule MeuModulo.Math do
   def soma(parametro1, parametro2) do
     parametro1 + parametro2
   end
+
+  def zero?(0), do: true
+  def zero?(num) when is_integer(num), do: false
 end
