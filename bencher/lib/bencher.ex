d9b072574825221ad864bbd26e039c3889645dbc
ogabriel <gabrieloliver8991@gmail.com>
2023-06-02 12:11:18 -0300

    update

--- bencher/lib/bencher.ex
+++ bencher/lib/bencher.ex
@@ -2,17 +2,4 @@ defmodule Bencher do
   @moduledoc """
   Documentation for `Bencher`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bencher.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
